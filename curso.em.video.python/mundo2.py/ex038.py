6dfbe956935450e80a7fb8a1fe47180e152558af
Caroline Assis <caroolassissss@gmail.com>
2023-05-16 10:39:16 -0300

    save

---
+++ curso.em.video.python/mundo2.py/ex038.py
@@ -0,0 +1,12 @@
+#N maior menor ou igual 
+
+x = int(input('Digite o priemiro número: '))
+y = int(input('Digite o segundo número: '))
+
+if x > y:
+    print('O primeiro número é maior!')
+elif y > x: 
+    print('O segundo número é maior!')
+else:
+    print('Os números sao iguais!')
+    
