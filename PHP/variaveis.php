417f5451cc8d44b2da2efcc29e2bdc21a2acb5a0
Caroline Assis <caroolassissss@gmail.com>
2024-02-26 08:56:24 -0300

    comeco php

---
+++ PHP/variaveis.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Variaveis PHP</title>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+        $idade = 3;
+        $salario = 1825.54;
+        $nome = "Leonardo";
+        $casado = false; 
+    ?>
+</body>
+</html>
